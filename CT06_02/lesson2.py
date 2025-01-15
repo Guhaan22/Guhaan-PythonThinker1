d51d7b93405be53252715444004bf32a1bf72489
Guhaan22 <lakshmi.rbala@gmail.com>
2025-01-15 09:05:01 +0000

    1.2

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -2,6 +2,11 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
+set counter to 0
+repeat until counter = 50
+move 10 steps
+turn clockwise by 15 degrees
+change counter by 10
 
 num1 - int(input("num1"))
 num2 - int(input("num2"))       
@@ -12,3 +17,53 @@ ans = num1 + num2
 print(ans)
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+ask what is your age and assign the response to variable "answer"
+if "answer" is lesser than 18, say access denied else say welcome
+
+
+
+# Task: Write down the steps on how to solve the problem below
+
+# Design pseudocode for a recycling robot that sorts items
+# into bins for glass, plastic, and paper. The robot should
+# check each item's material and place it in the correct bin.
+repeat forever
+if item is a glass item, dispose it into glass recycling bin
+if item is a plastic item, dispose it into the plastic recycling bin
+if item is a paper item, dispose it into the paper recycling bin
+if item is not any of the three, dispose it into the incinerator.
+
+create a list and assign it to "glass_bin" 
+create a list and assign it to "plastic_bin" 
+create a list and assign it to "paper_bin" 
+
+if item == glass
+    glass_bin.
+else
+
+    start with 1 and go up to ten
+
+    if answer equals to 7 then say yippee
+    if answer is not equal to 7 then say try again
+
+    ..
+    
+# Task: Using pseudocode, design a program that will check if the
+# user has entered the right secret phrase.
+
+# 1. If the user has entered a wrong secret phrase, send them an
+# error message.
+# 1. If the user has entered the right secret phrase, congratulate
+# them.\     Test 1 t
+o
+#  Task: Write a pseudocode that calculates the final score of a student
+# based on the following weightage:
+# _each test is scored upon 100_
+# Test 1 = 20%
+# Test 2 = 40%
+# Test 3 = 40%
+
+first, we multiply 100 times 3
+then we add the  percentages of all three tests
+after that we divide the student's combined score by 3
+Done Teacher Amos!
