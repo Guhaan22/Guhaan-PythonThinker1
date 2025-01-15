bcf18d10cdacab528f573bfe1c0ae8bc1ecb9167
Guhaan22 <lakshmi.rbala@gmail.com>
2025-01-15 08:05:11 +0000

    Jan 15, 2025, 8:05 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,6 +5,6 @@ print("Hello from lesson 2")
 num1 - int(input("num1"))
 num2 - int(input("num2"))       
 
-print(type())
+print(type(num1))
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
