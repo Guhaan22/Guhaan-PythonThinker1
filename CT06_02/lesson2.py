24105030da60d8ac247201eeaccd66483d30d047
Guhaan22 <lakshmi.rbala@gmail.com>
2025-01-15 08:04:36 +0000

    Jan 15, 2025, 8:04 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -3,6 +3,6 @@ print("Hello from lesson 2")
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 num1 - int(input("num1"))
-num2 - int(input())            
+num2 - int(input("num2"))            
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
