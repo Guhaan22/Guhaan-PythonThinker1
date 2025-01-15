fe1e4d408c1079735aedc3fcecb156ff44a0368d
Guhaan22 <lakshmi.rbala@gmail.com>
2025-01-15 08:05:22 +0000

    Jan 15, 2025, 8:05 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -6,5 +6,6 @@ num1 - int(input("num1"))
 num2 - int(input("num2"))       
 
 print(type(num1))
+print(type())
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
