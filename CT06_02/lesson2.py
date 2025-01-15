625f41c5215f4c4f6d6327bbb715dd3a67b9c655
Guhaan22 <lakshmi.rbala@gmail.com>
2025-01-15 07:59:06 +0000

    Jan 15, 2025, 7:59 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -2,6 +2,7 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
-user: input()
+user: input(5
+            )
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
