99cd3fa36a0b3eb958b51b1c7200d11c0c4c1839
Guhaan22 <lakshmi.rbala@gmail.com>
2025-01-22 07:53:54 +0000

    Jan 22, 2025, 7:53 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -8,3 +8,17 @@ print("Hello from lesson 3")
 # Brandon: 58
 # Charlie: 98
 # Daniel: 79
+# first the code will add 60 + 58 + 98 + 79 
+# Then, it will divide the sum by 4 since there are four numbers.
+# tada done the answer is................................................................................................................................................................................................................................................. 73.75
+
+# 1. Start
+# 2. Initialize total_score to 0
+# 3. Add Adam's score to total_score (total_score = total_score + 60)
+# 4. Add Brandon's score to total_score (total_score = total_score + 58)
+# 5. Add Charlie's score to total_score (total_score = total_score + 98)
+# 6. Add Daniel's score to total_score (total_score = total_score + 79)
+# 7. Calculate the number of students (num_students = 4)
+# 8. Calculate the average score (average_score = total_score / num_students)
+# 9. Print the average_score
+# 10. End
