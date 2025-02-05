981fb9fb817ba383e5232080251231dae34ec5a1
Guhaan22 <lakshmi.rbala@gmail.com>
2025-02-05 08:17:52 +0000

    Feb 5, 2025, 8:17 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -40,3 +40,24 @@ YourColour = input("What is your favorite colour?: ")
 print(YourColour)
 YourAge = input("What is your age?: ")
 print(YourAge)
+
+
+## Task 2: Input & string concatenation
+
+# **Task 2a**:
+# Ask the user for their name using input() and store it in a
+# variable. 
+# Concatenate the name with "Hi, [name]!" and print the
+# complete message.
+
+# **Task 2b**:
+# Use input() to ask the user for their favorite hobby. Store this
+# in a variable.
+# Print a message saying "I enjoy [hobby]" using string
+# concatenation.
+
+# **Task 2c**:
+# Ask the user for their dream vacation destination using input()
+# and store it in a variable.
+# Concatenate this variable with a phrase like "I would love to
+# visit" and print the full sentence.
