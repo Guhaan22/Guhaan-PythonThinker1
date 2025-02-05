b8e4c90628baa4befbb2816741fa994d81991123
Guhaan22 <lakshmi.rbala@gmail.com>
2025-02-05 08:59:43 +0000

    Feb 5, 2025, 8:59 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -90,4 +90,5 @@
 # years old".What is your age" )
 YourAge3 = int(input("what is your age"))
 print("Next year you will be" + YourAge3 + 1)
-YourNumber3 = int(input("what number would you like")
+YourNumber3 = int(input("what number would you like"))
+print("")
