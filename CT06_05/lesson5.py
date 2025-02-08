70bbd2ab921cccc25f31ae68981b8172af69f1b8
Guhaan22 <lakshmi.rbala@gmail.com>
2025-02-08 13:01:40 +0000

    hello

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,32 @@
 print("Hello from lesson 5")
+for i in range(100):
+         print("I Like Chicken Rice")
+
+for c in range(100):
+        print("I Like Cake")
+        print("Give Me More")
+for n in range(0, 60):
+        print(n)
+for bert in range(1, 6):
+        print(bert)
+for teacher in range(51, 101):
+        print(teacher)
+for teacher_amos in range(18, 30):
+        print(teacher_amos)
+for pie in range(2, 25, 2):
+        print(pie)
+for ms_mommy in range (8, 97, 8):
+        print(ms_mommy)
+for mr_daddy in range(5, 0, -1):
+        print(mr_daddy)
+
+print("Ready")
+for race in range(3, 0, -1):
+    print(race)
+for timer in range(10, 0, -1):
+        print(timer)
+print("Boo")
+  
+
+
+         
