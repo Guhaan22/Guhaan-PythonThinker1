b55802195a7258def4492c76da8b13603024f660
Guhaan22 <lakshmi.rbala@gmail.com>
2025-02-12 07:50:10 +0000

    Feb 12, 2025, 7:50 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -19,7 +19,7 @@
 my = int(input("What is the birthday person's name?"))
 by = int(input("What is the birthday person's age this year"))
 guy = int(input("What personal message you would like to say"))
-print
+print(Happy )
 
 
 
