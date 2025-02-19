049a8ab36919efbdc77b87293acff0d975c3305e
Guhaan22 <lakshmi.rbala@gmail.com>
2025-02-19 08:48:15 +0000

    Feb 19, 2025, 8:48 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -23,19 +23,36 @@
 ## Task 1: Syntax Errors
 # Fix the errors in the following:
 
-# **Task 1a**:
-for i in range(3):
-    print("Hello, World!")
+# # **Task 1a**:
+# for i in range(3):
+#     print("Hello, World!")
 
-# **Task 1b**:
-for i in range(5):
-    print(i)
+# # **Task 1b**:
+# for i in range(5):
+#     print(i)
 
-# **Task 1c**:
-print("Hello, World!")
+# # **Task 1c**:
+# print("Hello, World!")
 
-# **Task 1d**:
-hello = 5
+# # **Task 1d**:
+# hello = 5
 
-# **Task 1e**:
-print("Hello, World!")
+# # **Task 1e**:
+# print("Hello, World!")
+
+## Task 2: Name Errors
+# Fix the errors in the following:
+
+# **Task 2a**:
+print(age)
+
+# **Task 2b**:
+name = "Alice"
+print(nam)
+
+# **Task 2c**:
+print(x)
+x = 5
+
+# **Task 2d**:
+pint("Hello, World!")
