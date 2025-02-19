2a4704fcd2916087e7ad2c3c1b54ddd77de63af7
Guhaan22 <lakshmi.rbala@gmail.com>
2025-02-19 07:51:21 +0000

    Feb 19, 2025, 7:51 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,3 +1,18 @@
+# Lesson 6 - Debugging
+
+## Recap 1: Class Average Calculator
+
+# You have been tasked to create a programme that calculates the
+# average marks of a class.
+
+# Your programme will have to ask the
+# user for the total number of students, followed by the marks of
+# each student one at a time.
+
+# Use only variables, math operators that you have learnt, as
+# well as a 'for' loop.
+
+
 print("Hello from lesson 6")
 print("Hello World")
 print("Ready")
