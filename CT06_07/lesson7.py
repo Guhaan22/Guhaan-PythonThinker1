e71df0475a41f68a3e7ced8d6c5f4f9f89428da8
Guhaan22 <lakshmi.rbala@gmail.com>
2025-02-26 08:10:46 +0000

    Feb 26, 2025, 8:10 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -78,8 +78,8 @@
 # ## Task 3: Countdown From 10 For Loop
 
 # Use a 'for' loop and range() to count down from 10 to 1.
-for t in range(10, 0, -1):
-    print(t)
+# for t in range(10, 0, -1):
+#     print(t)
 
 ## Task 4: Word Repetition Input Loop
 
