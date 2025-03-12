b8a42fec6741386ec570ed6052e1665328a88d40
Guhaan22 <lakshmi.rbala@gmail.com>
2025-03-12 07:32:30 +0000

    Mar 12, 2025, 7:32 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,9 @@
 print("Hello from lesson 9")
+# The line below is asking the user for the number he wants to store as start
+start = input("What number should start be")
+# The line below is asking the user for the variable to be stored as end
+end = input("What number should your end be")
+# This line is asking the user for the increment
+increment = input("What number should the increment be")
+for i in range(int(start, end, increment)):
+    print(i)
