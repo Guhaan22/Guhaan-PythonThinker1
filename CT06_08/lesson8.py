b933b87d302fa514ad62377b11140f4132ebaaaf
Guhaan22 <lakshmi.rbala@gmail.com>
2025-03-12 07:37:33 +0000

    Mar 12, 2025, 7:37 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -8,4 +8,4 @@
 # 1. Using a for loop, ask the user for 5 numbers one at a time.
 # 2. Calculate the multiplication for these 5 numbers and print
 #    it out.
-num1 = input("")
+num1 = input("what is number" + t)
