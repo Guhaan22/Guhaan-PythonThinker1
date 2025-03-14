86f425505b304ade83e0c1e0c40599420bbf14d4
Guhaan22 <lakshmi.rbala@gmail.com>
2025-03-14 10:06:44 +0000

    Mar 14, 2025, 10:06 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -380,8 +380,8 @@
 # Import the 'random' library and create a program that randomly
 # output a number between 1 to 6
 import random
-random.randint(1, 6)
-print 
+num = random.randint(1, 6)
+print(num)
 
 # **Task 2b**:
 # Using the 'random' library, create 20 numbers between 0 and
