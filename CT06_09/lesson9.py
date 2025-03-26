884cd088798d682ee32e838150d62d32a3f36456
Guhaan22 <lakshmi.rbala@gmail.com>
2025-03-26 07:40:01 +0000

    Mar 26, 2025, 7:40 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,9 +1,29 @@
-print("Hello from lesson 9")
-# The line below is asking the user for the number he wants to store as start
-start = input("What number should start be")
-# The line below is asking the user for the variable to be stored as end
-end = input("What number should your end be")
-# This line is asking the user for the increment
-increment = input("What number should the increment be")
-for i in range(int(start, end, increment)):
-    print(i)
+# print("Hello from lesson 9")
+# # The line below is asking the user for the number he wants to store as start
+# start = input("What number should start be")
+# # The line below is asking the user for the variable to be stored as end
+# end = input("What number should your end be")
+# # This line is asking the user for the increment
+# increment = input("What number should the increment be")
+# for i in range(int(start, end, increment)):
+#     print(i)
+
+# Task 6: Flowchart for Temperature Monitor
+# You are analyzing daily temperature readings over a week.
+# Write a program to count how many days had a temperature
+# that is greater than 30.
+
+# Draw out the flowchart (on a piece of paper) of the above
+# program.
+
+# 1. Start with creating and assigning the variable
+#    "positive_days" to 0 before the loop.
+# 2. Use a for loop to iterate through each day of the week
+#    (7 times)
+# 3. In each iteration the loop, prompt the user to input the
+#    temperature for the day.
+# 4. Use an 'if' condition to check if the temperature is greater
+#    than 30. If so, increase the variable 'positive_days' by
+#    1
+# 5. After the loop, print the count of days with temperature
+#    higher than 30.
